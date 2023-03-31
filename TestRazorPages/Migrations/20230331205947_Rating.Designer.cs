d298c44afa7f7c94b3432dbc6e2f12723e9a069e
Max-pip <4640034max@gmail.com>
2023-04-01 00:05:07 +0300

    commit

--- TestRazorPages/Migrations/20230331205947_Rating.Designer.cs
+++ TestRazorPages/Migrations/20230331205947_Rating.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TestRazorPages.Data;
 
@@ -11,9 +12,11 @@ using TestRazorPages.Data;
 namespace TestRazorPages.Migrations
 {
     [DbContext(typeof(TestRazorPagesContext))]
-    partial class TestRazorPagesContextModelSnapshot : ModelSnapshot
+    [Migration("20230331205947_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
